d09325b3a1b0d95ae8904913c5f81059535a9090
Mateus Barros <mateus.rodrigues@geekie.com.br>
2022-10-25 15:44:09 -0300

    Changes icons again and receives fill color from props

--- src/components/iconography/sub-sup.js
+++ src/components/iconography/sub-sup.js
@@ -1,6 +1,11 @@
+const PropTypes = require("prop-types");
 const React = require("react");
 
 class SubSup extends React.Component {
+  static propTypes = {
+    color: PropTypes.string.isRequired,
+};
+
   render() {
     return (
       <svg
@@ -11,24 +16,23 @@ class SubSup extends React.Component {
         xmlns="http://www.w3.org/2000/svg"
       >
         <g clip-path="url(#clip0_337_606)">
-          <rect width="22" height="24" fill="white" />
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M0 5.25C0 4.55964 0.559645 4 1.25 4H10.75C11.4404 4 12 4.55965 12 5.25V18.75C12 19.4404 11.4404 20 10.75 20H1.25C0.559643 20 0 19.4404 0 18.75V5.25ZM2 6V18H10V6H2Z"
-            fill="#3C3E40"
+            fill={this.props.color}
           />
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M14 1.25C14 0.559645 14.5596 0 15.25 0H20.75C21.4404 0 22 0.559644 22 1.25V6.75C22 7.44036 21.4404 8 20.75 8H15.25C14.5596 8 14 7.44036 14 6.75V1.25ZM16 2V6H20V2H16Z"
-            fill="#3C3E40"
+            fill={this.props.color}
           />
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M14 17.25C14 16.5596 14.5596 16 15.25 16H20.75C21.4404 16 22 16.5596 22 17.25V22.75C22 23.4404 21.4404 24 20.75 24H15.25C14.5596 24 14 23.4404 14 22.75V17.25ZM16 18V22H20V18H16Z"
-            fill="#3C3E40"
+            fill={this.props.color}
           />
         </g>
         <defs>
